79ef9ec615c558bf779316a69123cb8bd0d8c839
nishuintern <nishuintern21@gmail.com>
2025-05-12 11:51:14 +0530

    updated

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -1,38 +1,55 @@
 import axios from "axios";
-import React from "react";
+import React, { useEffect } from "react";
 import { BASE_URL } from "../utils/constants";
 
 const Premium = () => {
-    const handleBuyClick =async (type) => {
-        const order=await axios.post(
-            BASE_URL+'/payment/create',{
-                membershipType:type
-            },
-            {withCredentials:true}
-        )
-        const { amount, keyId, currency, notes, orderId } = order.data;
+  const [isUserPremium, setIsUserPremium] = React.useState(false);
+  useEffect(() => {
+    verifyPremiumUser();
+  }, []);
 
-        const options = {
-          key: keyId,
-          amount,
-          currency,
-          name: "Dev Tinder",
-          description: "Connect to other developers",
-          order_id: orderId,
-          prefill: {
-            name: notes.firstName + " " + notes.lastName,
-            email: notes.emailId,
-            contact: "9999999999",
-          },
-          theme: {
-            color: "#F37254",
-          },
-          // handler: verifyPremiumUser,
-        };
-        const  rzp=new window.Razorpay(options);
-        rzp.open();
+  const verifyPremiumUser = async () => {
+    const res = await axios.get(BASE_URL + "/premium/verify", {
+      withCredentials: true,
+    });
+    if (res.data.isPremium) {
+      setIsUserPremium(true);
+    }
+  };
+
+  const handleBuyClick = async (type) => {
+    const order = await axios.post(
+      BASE_URL + "/payment/create",
+      {
+        membershipType: type,
+      },
+      { withCredentials: true }
+    );
+    const { amount, keyId, currency, notes, orderId } = order.data;
+
+    const options = {
+      key: keyId,
+      amount,
+      currency,
+      name: "Dev Tinder",
+      description: "Connect to other developers",
+      order_id: orderId,
+      prefill: {
+        name: notes.firstName + " " + notes.lastName,
+        email: notes.emailId,
+        contact: "9999999999",
+      },
+      theme: {
+        color: "#F37254",
+      },
+      handler: verifyPremiumUser,
     };
-  return (
+    const rzp = new window.Razorpay(options);
+    rzp.open();
+  };
+  return isUserPremium ? (
+    "You are already a premium member"
+  ) : (
     <div className="m-10">
       <div className="flex w-full">
         <div className="card bg-base-300 rounded-box grid h-80 grow place-items-center">
@@ -43,7 +60,12 @@ const Premium = () => {
             <li> - Blue Tick</li>
             <li> - 3 months </li>
           </ul>
-          <button onClick={()=>handleBuyClick('silver')} className="btn btn-secondary">Buy Silver</button>
+          <button
+            onClick={() => handleBuyClick("silver")}
+            className="btn btn-secondary"
+          >
+            Buy Silver
+          </button>
         </div>
         <div className="divider divider-horizontal">OR</div>
         <div className="card bg-base-300 rounded-box grid h-80 grow place-items-center">
@@ -54,7 +76,12 @@ const Premium = () => {
             <li> - Blue Tick</li>
             <li> - 6 months </li>
           </ul>
-          <button onClick={()=>handleBuyClick('gold')} className="btn btn-primary">Buy Gold</button>
+          <button
+            onClick={() => handleBuyClick("gold")}
+            className="btn btn-primary"
+          >
+            Buy Gold
+          </button>
         </div>
       </div>
     </div>
