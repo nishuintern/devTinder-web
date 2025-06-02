5c04ca62542dc1ca7ec0f3026127d399b7674571
nishuintern <nishuintern21@gmail.com>
2025-06-02 22:44:19 +0530

    update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
-export const BASE_URL =
-  location.hostname === "localhost"
-    ? "http://localhost:8000"
-    : "/api";
+// export const BASE_URL =
+//   location.hostname === "localhost"
+//     ? "http://localhost:8000"
+//     : "/api";
 
-// export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://devtinder-b1j9.onrender.com";
