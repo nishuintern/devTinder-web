d16b9e54245f53127c548154dc25a88ba0b29742
nishuintern <nishuintern21@gmail.com>
2025-06-03 12:02:43 +0530

    up

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const BASE_URL =
-  location.hostname === "localhost" ? "http://localhost:8000" : "/api";
+  location.hostname === "localhost" ? "http://localhost:7777" : "/api";
 
 // export const BASE_URL = "http://localhost:8000";
